c1cf3cf820b0014a13775b1b986f944e8dba221b
Jeff <jf.gasp@gmail.com>
2021-09-26 16:32:28 +0200

    config db ok

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+require('dotenv').config({ path: './config/.env' }); // Récupération variables d'environnement
+require('./config/db'); // Récupération de la connexion
 const app = express();
 
-// == On écoute sur le port 5000
-app.listen(5000, () => {
-    console.log('Listening on port 5000');
+// == On écoute sur le port PORT
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on port ${process.env.PORT}`);
 });
