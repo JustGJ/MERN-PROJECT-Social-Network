34413acf5485901224952d9b739c6e859e61b800
Jeff <jf.gasp@gmail.com>
2021-10-24 17:06:34 +0200

    like/unlike finished

--- client/src/components/Post/Card.js
+++ client/src/components/Post/Card.js
@@ -1,19 +1,34 @@
 import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
-import FollowHandler from '../Profil/FollowHandler';
+import { useDispatch, useSelector } from 'react-redux';
 import { dateParser, isEmpty } from '../Utils';
+import FollowHandler from '../Profil/FollowHandler';
+import LikeButton from './LikeButton';
+// import { updatePost } from '../../redux/actions/post.actions';
+// import DeleteCard from './DeleteCard';
+// import CardComments from './CardComments';
 
 const Card = ({ post }) => {
     const [isLoading, setIsLoading] = useState(true);
+    const [isUpdated, setIsUpdated] = useState(false);
+    const [textUpdate, setTextUpdate] = useState(null);
+    const [showComments, setShowComments] = useState(false);
     const usersData = useSelector((state) => state.usersReducer);
-    const userData = useSelector((state) => state.usersReducer);
+    const userData = useSelector((state) => state.userReducer);
+    const dispatch = useDispatch();
+
+    // const updateItem = () => {
+    //     if (textUpdate) {
+    //         dispatch(updatePost(post._id, textUpdate));
+    //     }
+    //     setIsUpdated(false);
+    // };
 
     useEffect(() => {
         !isEmpty(usersData[0]) && setIsLoading(false);
     }, [usersData]);
 
     return (
-        <li key={post._id} className="card-container">
+        <li className="card-container" key={post._id}>
             {isLoading ? (
                 <i className="fas fa-spinner fa-spin"></i>
             ) : (
@@ -25,10 +40,9 @@ const Card = ({ post }) => {
                                 !isEmpty(usersData[0]) &&
                                 usersData
                                     .map((user) => {
-                                        // Si l'user parcouru a posté un post, alors on return sa picture
                                         if (user._id === post.posterId) return user.picture;
+                                        else return null;
                                     })
-                                    // Pour retirer toutes les virgules
                                     .join('')
                             }
                             alt="poster-pic"
@@ -40,18 +54,33 @@ const Card = ({ post }) => {
                             <div className="pseudo">
                                 <h3>
                                     {!isEmpty(usersData[0]) &&
-                                        usersData.map((user) => {
-                                            // Si l'user parcouru a posté un post, alors on return son pseudo
-                                            if (user._id === post.posterId) return user.pseudo;
-                                        })}
+                                        usersData
+                                            .map((user) => {
+                                                if (user._id === post.posterId) return user.pseudo;
+                                                else return null;
+                                            })
+                                            .join('')}
                                 </h3>
                                 {post.posterId !== userData._id && (
-                                    <FollowHandler idToFollow={post.posterId} type="card" />
+                                    <FollowHandler idToFollow={post.posterId} type={'card'} />
                                 )}
                             </div>
                             <span>{dateParser(post.createdAt)}</span>
                         </div>
-                        <p>{post.message}</p>
+                        {isUpdated === false && <p>{post.message}</p>}
+                        {isUpdated && (
+                            <div className="update-post">
+                                <textarea
+                                    defaultValue={post.message}
+                                    onChange={(e) => setTextUpdate(e.target.value)}
+                                />
+                                {/* <div className="button-container">
+                                    <button className="btn" onClick={updateItem}>
+                                        Valider modification
+                                    </button>
+                                </div> */}
+                            </div>
+                        )}
                         {post.picture && (
                             <img src={post.picture} alt="card-pic" className="card-pic" />
                         )}
@@ -65,14 +94,28 @@ const Card = ({ post }) => {
                                 allowFullScreen
                                 title={post._id}></iframe>
                         )}
-                    </div>
-                    <div className="card-footer">
-                        <div className="comment-icon">
-                            <img src="./img/icons/message1.svg" alt="comment" />
-                            <span>{post.comments.length}</span>
+                        {userData._id === post.posterId && (
+                            <div className="button-container">
+                                <div onClick={() => setIsUpdated(!isUpdated)}>
+                                    <img src="./img/icons/edit.svg" alt="edit" />
+                                </div>
+                                {/* <DeleteCard id={post._id} /> */}
+                            </div>
+                        )}
+                        {/* AFFICHAGE FOOTER */}
+                        <div className="card-footer">
+                            <div className="comment-icon">
+                                <img
+                                    onClick={() => setShowComments(!showComments)}
+                                    src="./img/icons/message1.svg"
+                                    alt="comment"
+                                />
+                                <span>{post.comments.length}</span>
+                            </div>
+                            <LikeButton post={post} />
+                            <img src="./img/icons/share.svg" alt="share" />
                         </div>
-                        <h6>Like button</h6>
-                        <img src="./img.icons/share.svg" alt="share" />
+                        {/* {showComments && <CardComments post={post} />} */}
                     </div>
                 </>
             )}
--- client/src/redux/reducers/post.reducer.js
+++ client/src/redux/reducers/post.reducer.js
@@ -1,4 +1,4 @@
-import { GET_POST } from '../actions/post.actions';
+import { GET_POST, LIKE_POST, UNLIKE_POST } from '../actions/post.actions';
 
 const initialState = {};
 
@@ -6,7 +6,26 @@ export default function userReducer(state = initialState, action) {
     switch (action.type) {
         case GET_POST:
             return action.payload;
-
+        case LIKE_POST:
+            return state.map((post) => {
+                if (post._id === action.payload.postId) {
+                    return {
+                        ...post,
+                        likers: [action.payload.userId, ...post.likers],
+                    };
+                }
+                return post;
+            });
+        case UNLIKE_POST:
+            return state.map((post) => {
+                if (post._id === action.payload.postId) {
+                    return {
+                        ...post,
+                        likers: post.likers.filter((id) => id !== action.payload.userId),
+                    };
+                }
+                return post;
+            });
         default:
             return state;
     }
