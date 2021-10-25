4c0f4dbd516011e6fa8978027449e1a41878984f
Jeff <jf.gasp@gmail.com>
2021-10-25 21:13:36 +0200

    bug fixed model post

--- server/models/post.model.js
+++ server/models/post.model.js
@@ -25,7 +25,7 @@ const PostSchema = new mongoose.Schema(
             type: [
                 {
                     commenterId: String,
-                    commentPseudo: String,
+                    commenterPseudo: String,
                     text: String,
                     timestamp: Number,
                 },
